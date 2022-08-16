7bd0c419fa771d7748649a0a8e211e46a9567b85
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-16 23:00:10 +0530

    [Hemant] Added UC1 find max integer with passed test cases

--- Generic/Program.cs
+++ Generic/Program.cs
@@ -6,7 +6,35 @@ namespace Generic
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generic Problem");
+            Console.WriteLine("Welcome to Generics Program");
+
+            bool check = true;
+            FindMax obj = new FindMax();
+
+
+            while (check)
+            {
+                Console.Write("Select Option:\n" +
+                   "1)Find Integer Maximum Number\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+
+
+                    case 1:
+                        int result = obj.FindMaxInteger(23, 50, 5);
+
+                        Console.WriteLine(result);
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::");
+                        Console.WriteLine("Greatest number among three numbers is :" + result);
+                        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::::::");
+                        break;
+                    default:
+                        check = !check;
+                        break;
+                }
+            }
+
         }
     }
 }
